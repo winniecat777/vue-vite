957128a3f2408cd95f118b0d9bf553a3a9bd90d1
winniecat777 <winniecat777@gmail.com>
2024-01-31 22:49:12 +0800

    git add

---
+++ src/views/AboutView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ data }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: {}
+    }
+  },
+  mounted() {
+    console.log(import.meta.env.VITE_PATH);
+    // import.meta.env.VITE_PATH 在 vite 專案 載入 .env 的環境變數
+    const url = import.meta.env.VITE_PATH;
+    // console.log(import.meta.env.VITE_TEXT);
+    this.$http.get(url)
+      .then((res) => {
+        console.log(res);
+        this.data = res.data.results[0];
+        console.log(this.data);
+      })
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
